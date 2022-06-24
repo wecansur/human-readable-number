d3da8a6289e53cccc10de5a2e2bd095f5254413c
wecansur <vlkob95@gmail.com>
2022-06-24 16:19:15 +0800

    feat: create initial cycle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
-  
+  const numberStr = String(number);
+  let result = '';
+
+  for (let i = numberStr[numberStr.length -1]; i >= 0; i--) {
+
+  }
 }
